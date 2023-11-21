6f7897f285ae963be795e15e642709e31dabf977
yasin <yasinmiapolash@gmail.com>
2023-11-21 22:46:37 +0600

    13

--- lib/ui/screen/edit_profile_screen.dart
+++ lib/ui/screen/edit_profile_screen.dart
@@ -18,6 +18,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
           children: [
             ProfileSummaryCard(
 
+
             ),
             Expanded(
                 child: BodyBackground(
